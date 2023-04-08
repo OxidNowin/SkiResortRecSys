6f7d873250a18cb871696789ef2fc95808fa8c25
OxidNowin <terror20007@mail.ru>
2023-04-08 11:20:07 +0300

    add dotenv package

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
 # - *- coding: utf- 8 - *-
 import os
+from dotenv import load_dotenv, find_dotenv
+
+
+load_dotenv(find_dotenv())
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 OPENAI_KEY = os.getenv('OPENAI_KEY')
 DB_NAME = os.getenv('DB_NAME')
-USER = os.getenv('USER')
+USER = os.getenv('USER_BD')
 PASSWORD = os.getenv('PASSWORD')
 HOST = os.getenv('HOST')
 BOT_ADMIN = os.getenv('BOT_ADMIN')
